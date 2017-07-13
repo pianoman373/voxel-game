99814f35207dd18b4e69a9818a0a3a54cf05bef7
pianoman373 <jholley373@yahoo.com>
2017-07-13 09:12:37 -0500

    Update block colors.

--- init.lua
+++ init.lua
@@ -33,7 +33,7 @@ registerBlock(0, {
 
 registerBlock(1, {
 	name = "Grass",
-	color = {78.0/255.0, 111.0/255.0, 40.0/255.0},
+	color = {78/255, 111/255, 40/255},
 	getTextureCoord = function(side)
 		if side == EnumDirection.POSITIVE_Y then
 			return 0, 0
@@ -48,19 +48,19 @@ registerBlock(1, {
 	end
 })
 
-registerBlock(2, simpleBlock("Stone", 1, 0, {138.0/255.0, 128.0/255.0, 115.0/255.0}))
+registerBlock(2, simpleBlock("Stone", 1, 0, {138/255, 128/255, 115/255}))
 
-registerBlock(3, simpleBlock("Dirt", 2, 0, {0.0, 0.0, 0.0}))
+registerBlock(3, simpleBlock("Dirt", 2, 0, {73/255, 51/255, 41/255}))
 
-registerBlock(4, simpleBlock("Cobblestone", 0, 1, {0.0, 0.0, 0.0}))
+registerBlock(4, simpleBlock("Cobblestone", 0, 1, {148/255, 138/255, 129/255}))
 
-registerBlock(5, simpleBlock("Planks", 4, 0, {0.0, 0.0, 0.0}))
+registerBlock(5, simpleBlock("Planks", 4, 0, {106/255, 85/255, 61/255}))
 
-registerBlock(6, simpleBlock("Wood", 4, 1, {0.0, 0.0, 0.0}))
+registerBlock(6, simpleBlock("Wood", 4, 1, {87/255, 63/255, 52/255}))
 
 registerBlock(7, {
 	name = "Leaves",
-	color = {0.0, 0.0, 0.0},
+	color = {91/255, 129/255, 48/255},
 	getTextureCoord = function(side)
 		return 4, 3
 	end,
@@ -69,13 +69,13 @@ registerBlock(7, {
 	end
 })
 
-registerBlock(8, simpleBlock("Bricks", 7, 0, {0.0, 0.0, 0.0}))
+registerBlock(8, simpleBlock("Bricks", 7, 0, {161/255, 149/255, 134/255}))
 
-registerBlock(9, simpleBlock("Stone Bricks", 6, 3, {0.0, 0.0, 0.0}))
+registerBlock(9, simpleBlock("Stone Bricks", 6, 3, {129/255, 93/255, 70/255}))
 
 registerBlock(10, {
 	name = "Glass",
-	color = {0.0, 0.0, 0.0},
+	color = {216/255, 227/255, 231/255},
 	getTextureCoord = function(side)
 		return 1, 3
 	end,
