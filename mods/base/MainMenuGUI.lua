a9d3a9e07ac2d3b56cfbb23566fc36cfd920ea8e
jholley <jholley373@tamu.edu>
2020-03-20 18:51:32 -0500

    stuff

--- mods/base/MainMenuGUI.lua
+++ mods/base/MainMenuGUI.lua
@@ -16,11 +16,15 @@ end
 local background = api.getTexture("base:resources/textures/background.png")
 
 local panel = newGUI.panel()
-panel:addChild(newGUI.sprite, {x=0.2, y=0.2, width=0.1, height=0.1}, {color={r=1.0,g=0.0,b=0.0,a=1.0}})
+panel:addChild(newGUI.sprite, {x=0.15, y=0.5, width=0.2, height=0.9}, {color={r=0.2,g=0.2,b=0.2,a=1.0}})
+local panel2 = panel:addPanel({x=0.15, y=0.5, width=0.2, height=0.9})
+
+panel2:addChild(newGUI.button, {x=0.5, y=0.1, width=0.8, height=0.1}, {color={r=0.5,g=0.5,b=1.0,a=1.0}, text="button 1"})
+panel2:addChild(newGUI.button, {x=0.5, y=0.25, width=0.8, height=0.1}, {color={r=0.5,g=0.5,b=1.0,a=1.0}, text="button 2"})
 
 
 function MainMenuGUI:render(width, height)
-    panel:render(width, height)
+    panel:renderToScreen(width, height)
     -- api.renderSpriteTexture(0, 0, width, height, 0, 0, 1, 1, background)
 
     -- self.addressBox:render(0, 150, 200, 30)
--- mods/base/newGUI.lua
+++ mods/base/newGUI.lua
@@ -1,14 +1,14 @@
 
 local newGUI = {}
 
-local function getDimensions(constraints, panelWidth, panelHeight)
+local function getDimensions(constraints, panelX, panelY, panelWidth, panelHeight)
     local width = 128
     local height = 128
     local x = 0
     local y = 0
 
-    x = panelWidth * constraints.x
-    y = panelHeight * constraints.y
+    x = (panelX - (panelWidth / 2)) + (panelWidth * constraints.x)
+    y = (panelY - (panelHeight / 2)) + (panelHeight * constraints.y)
     width = panelWidth * constraints.width
     height = panelHeight * constraints.height
 
@@ -21,11 +21,20 @@ panel.__index = panel
 
 
 -- user friendly render function
-function panel:render(panelWidth, panelHeight)
+function panel:renderToScreen(panelWidth, panelHeight)
     for _, child in ipairs(self.children) do
-        x, y, width, height = getDimensions(child.constraints, panelWidth, panelHeight)
+        cx, cy, cwidth, cheight = getDimensions(child.constraints, panelWidth / 2, panelHeight / 2, panelWidth, panelHeight)
 
-        child.type.render(x, y, width, height, child.data)
+        child.type.render(cx, cy, cwidth, cheight, child.data)
+    end
+end
+
+-- internal render function
+function panel.render(x, y, width, height, data)
+    for _, child in ipairs(data.children) do
+        cx, cy, cwidth, cheight = getDimensions(child.constraints, x, y, width, height)
+
+        child.type.render(cx, cy, cwidth, cheight, child.data)
     end
 end
 
@@ -34,7 +43,11 @@ function panel:addChild(childType, constraints, data)
 end
 
 function panel:addPanel(constraints)
-    
+    local panelInstance = setmetatable({children = {}}, panel)
+
+    table.insert(self.children, {type=panel, constraints=constraints, data=panelInstance})
+
+    return panelInstance
 end
 
 
@@ -47,7 +60,32 @@ end
 newGUI.sprite = {}
 
 function newGUI.sprite.render(x, y, width, height, data)
-    api.renderSprite(x - (width/2), y - (width/2), width, height, data.color.r, data.color.g, data.color.b, data.color.a)
+    api.renderSprite(x - (width/2), y - (height/2), width, height, data.color.r, data.color.g, data.color.b, data.color.a)
+end
+
+newGUI.button = {}
+
+local font = api.getFont("base:resources/HelvetiPixel.ttf")
+
+function newGUI.drawText(text, x, y, r, g, b, a)
+    api.renderText(x+2, y-2, text, font, 0, 0, 0, a or 1)
+    api.renderText(x, y, text, font, r or 1, g or 1, b or 1, a or 1)
+end
+
+function newGUI.drawTextCentered(text, x, y, r, g, b, a)
+    local textSize = font:getTextSize(text)
+
+    x = x - (textSize.x/2)
+    y = y - (textSize.y/2)
+
+    api.renderText(x+2, y-2, text, font, 0, 0, 0, a or 1)
+    api.renderText(x, y, text, font, r or 1, g or 1, b or 1, a or 1)
+end
+
+function newGUI.button.render(x, y, width, height, data)
+    api.renderSprite(x - (width/2), y - (height/2), width, height, data.color.r, data.color.g, data.color.b, data.color.a)
+
+    newGUI.drawTextCentered(data.text, x, y, 1.0, 1.0, 1.0, 1.0)
 end
 
 return newGUI
